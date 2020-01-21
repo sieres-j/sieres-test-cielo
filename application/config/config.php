cdc7b446b3890e4ca28aff4663acee0468d3a6e7
sieres-j <60114010+sieres-j@users.noreply.github.com>
2020-01-21 18:38:49 -0500

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8080/sieres-cielo-test';
+$config['base_url'] = 'http://localhost:80/sieres-test-cielo-master';
 
 /*
 |--------------------------------------------------------------------------
